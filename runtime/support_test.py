ce6323d3a9a2eb0e10999df1142129277612c94c
Joshua Chorlton <jac@stripe.com>
2017-11-16 12:18:33 -0800

    Managed to eliminate deps
    Signed-off-by: Yunlong Liu <davislong198833@gmail.com>

--- runtime/support_test.py
+++ runtime/support_test.py
@@ -13,10 +13,10 @@
 # limitations under the License.
 
 import io
+import os
 import sys
-import unittest
-import mock
 import tempfile
+import unittest
 import zipfile
 
 from subpar.runtime import support
@@ -44,17 +44,22 @@ class SupportTest(unittest.TestCase):
         path = support._find_archive()
         self.assertNotEqual(path, None)
 
-    # patch zipfile because there is no real zipfile
-    @mock.patch('zipfile.ZipFile')
-    def test_setup(self, ZipFileMock):
-        # test when there are no .so files in the archive
-        ZipFileMock.return_value.__enter__.return_value.namelist.return_value = [
+    def test_setup(self):
+        # mock out finding the archive to some other archive
+        archive_path = self._make_archive([
             'some_root/main.py',
             'another_root/main.py'
-        ]
+        ])
+        def mock_find_archive():
+            return archive_path
+        old_find_archive = support._find_archive
+        support._find_archive = mock_find_archive
+
+        # mock out sys path
         old_sys_path = sys.path
         mock_sys_path = list(sys.path)
         sys.path = mock_sys_path
+
         # `import pip` can cause arbitrary sys.path changes,
         # especially if using the Debian `python-pip` package or
         # similar.  Do it first to get those changes out of the
@@ -73,31 +78,42 @@ class SupportTest(unittest.TestCase):
             sys.path = mock_sys_path
             support.setup(import_roots=['some_root', 'another_root'])
         finally:
+            support._find_archive = old_find_archive
             sys.path = old_sys_path
 
         # Check results
-        self.assertTrue(mock_sys_path[1].endswith('subpar/runtime/some_root'),
+        self.assertTrue(mock_sys_path[1].endswith('{}/some_root'.format(archive_path)),
                         mock_sys_path)
         self.assertTrue(
-            mock_sys_path[2].endswith('subpar/runtime/another_root'),
+            mock_sys_path[2].endswith('{}/another_root'.format(archive_path)),
             mock_sys_path)
         self.assertEqual(mock_sys_path[0], sys.path[0])
         self.assertEqual(mock_sys_path[3:], sys.path[1:])
 
-        # Make sure nothing got extracted, since there are no shared object files
-        ZipFileMock.return_value.__enter__.return_value.extract.assert_not_called()
-
-    @mock.patch.object(tempfile, 'mkdtemp', return_value='/some_tmp_dir')
-    @mock.patch('zipfile.ZipFile')
-    def test_setup_with_shared_objects(self, ZipFileMock, mkdtemp_mock):
-        ZipFileMock.return_value.__enter__.return_value.namelist.return_value = [
+    def test_setup_with_shared_objects(self):
+        # mock out finding the archive to some other archive
+        archive_path = self._make_archive([
             'some_root/main.py',
             'some_root/file.so',
             'another_root/main.py'
-        ]
+        ])
+        def mock_find_archive():
+            return archive_path
+        old_find_archive = support._find_archive
+        support._find_archive = mock_find_archive
+
+        # mock out creating a tmp dir to a predictable tmp dir
+        tempdir = tempfile.mkdtemp()
+        def mock_mkdtemp():
+            return tempdir
+        old_mkdtemp = tempfile.mkdtemp
+        tempfile.mkdtemp = mock_mkdtemp
+
+        # mock out the sys path
         old_sys_path = sys.path
         mock_sys_path = list(sys.path)
         sys.path = mock_sys_path
+
         # `import pip` can cause arbitrary sys.path changes,
         # especially if using the Debian `python-pip` package or
         # similar.  Do it first to get those changes out of the
@@ -116,25 +132,32 @@ class SupportTest(unittest.TestCase):
             sys.path = mock_sys_path
             support.setup(import_roots=['some_root', 'another_root'])
         finally:
+            support._find_archive = old_find_archive
+            tempfile.mkdtemp = old_mkdtemp
             sys.path = old_sys_path
 
         # Check results
         # some_root should be extracted to the tmp dir since
         # it has shared object files
-        self.assertEqual(mock_sys_path[1], '/some_tmp_dir/some_root')
-        calls = [
-            mock.call('some_root/main.py', '/some_tmp_dir'),
-            mock.call('some_root/file.so', '/some_tmp_dir')
-        ]
-        ZipFileMock.return_value.__enter__.return_value.extract.assert_has_calls(calls)
+        self.assertEqual(mock_sys_path[1], '{}/some_root'.format(tempdir))
+        files_in_tmp_dir = os.listdir('{}/some_root'.format(tempdir))
+        self.assertIn('main.py', files_in_tmp_dir)
+        self.assertIn('file.so', files_in_tmp_dir)
 
         # another_root should just be added to sys.path since
         # there is no reason to extract it
         self.assertTrue(
-            mock_sys_path[2].endswith('subpar/runtime/another_root'),
+            mock_sys_path[2].endswith('{}/another_root'.format(archive_path)),
             mock_sys_path)
         self.assertEqual(mock_sys_path[0], sys.path[0])
         self.assertEqual(mock_sys_path[3:], sys.path[1:])
+
+    def _make_archive(self, filenames):
+        _, archive_file = tempfile.mkstemp(suffix='.zip')
+        with zipfile.ZipFile(archive_file, 'w') as archive:
+            for f in filenames:
+                archive.writestr(f, 'print("hello world")')
+        return archive_file
 
 
 if __name__ == '__main__':
