78576bddd65446c72c5af004794a0266e04d91b5
Douglas Greiman <dgreiman@google.com>
2018-01-16 15:50:02 -0800

    Support extracting .par file contents to disk. (#65)
    Useful for importing C extension modules, or for data resources that
    must be real files, instead of file-like objects.
    
    To use, add the zip_safe=False attribute to a par_binary() rule.
    
    Yes, the name zip_safe is strange, but it's consistent with Python
    distutils and eggs.

--- runtime/support_test.py
+++ runtime/support_test.py
@@ -13,13 +13,69 @@
 # limitations under the License.
 
 import io
+import os
 import sys
+import tempfile
 import unittest
+import zipfile
 
+from subpar.compiler import test_utils
 from subpar.runtime import support
 
+# `import pip` can cause arbitrary sys.path changes,
+# especially if using the Debian `python-pip` package or
+# similar.  Do it first to get those changes out of the
+# way.
+old_sys_path = sys.path
+try:
+    mock_sys_path = list(sys.path)
+    sys.path = mock_sys_path
+    import pip  # noqa
+except ImportError:
+    pass
+finally:
+    sys.path = old_sys_path
 
+
+# We assume this test isn't run as a par file.
 class SupportTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Create zipfile for tests to read
+        tmpdir = test_utils.mkdtemp()
+        zipfile_name = os.path.join(tmpdir, '_support_test_sample.par')
+
+        z = zipfile.ZipFile(zipfile_name, 'w')
+        entry_name = '_support_test_helper_script.py'
+        entry_data = b'print("Hello world")'
+        z.writestr(entry_name, entry_data)
+
+        z.close()
+
+        cls.zipfile_name = zipfile_name
+        cls.entry_name = entry_name
+        cls.entry_data = entry_data
+
+        # Create mock loader object
+        class MockLoader(object): pass
+        mock_loader = MockLoader()
+        mock_loader.archive = zipfile_name
+        mock_loader.prefix = ''
+        main = sys.modules.get('__main__')
+        old_loader = getattr(main, '__loader__', None)
+        main.__loader__ = mock_loader
+
+        cls.mock_loader = mock_loader
+        cls.old_loader = old_loader
+
+    @classmethod
+    def tearDownClass(cls):
+        # Cleanup zipfile
+        os.remove(cls.zipfile_name)
+
+        # Cleanup loader mock
+        main = sys.modules.get('__main__')
+        main.__loader__ = cls.old_loader
 
     def test__log(self):
         old_stderr = sys.stderr
@@ -38,8 +94,22 @@ class SupportTest(unittest.TestCase):
 
     def test__find_archive(self):
         # pylint: disable=protected-access
-        path = support._find_archive()
-        self.assertNotEqual(path, None)
+        archive_path = support._find_archive()
+
+        self.assertNotEqual(archive_path, None)
+        self.assertTrue(zipfile.is_zipfile(archive_path))
+
+    def test__extract_files(self):
+        # Extract zipfile
+        extract_path = support._extract_files(self.zipfile_name)
+
+        # Check results
+        self.assertTrue(os.path.isdir(extract_path))
+        extracted_file = os.path.join(extract_path, self.entry_name)
+        self.assertTrue(os.path.isfile(extracted_file))
+        with open(extracted_file, 'rb') as f:
+            actual_data = f.read()
+            self.assertEqual(actual_data, self.entry_data)
 
     def test__version_check(self):
         class MockModule(object): pass
@@ -60,41 +130,47 @@ class SupportTest(unittest.TestCase):
         self.assertTrue(support._version_check_pkg_resources(pkg_resources))
 
     def test_setup(self):
-        old_sys_path = sys.path
-        mock_sys_path = list(sys.path)
-        sys.path = mock_sys_path
-        # `import pip` can cause arbitrary sys.path changes,
-        # especially if using the Debian `python-pip` package or
-        # similar.  Do it first to get those changes out of the
-        # way.
-        try:
-            import pip  # noqa
-        except ImportError:
-            pass
-        finally:
-            sys.path = old_sys_path
-
-        # Run setup()
+        # Run setup() without file extraction
         old_sys_path = sys.path
         try:
             mock_sys_path = list(sys.path)
+            mock_sys_path[0] = self.zipfile_name
             sys.path = mock_sys_path
-            support.setup(import_roots=['some_root', 'another_root'])
+            success = support.setup(import_roots=['some_root', 'another_root'], zip_safe=True)
+            self.assertTrue(success)
         finally:
             sys.path = old_sys_path
 
         # Check results
-        self.assertTrue(mock_sys_path[1].endswith('subpar/runtime/some_root'),
-                        mock_sys_path)
         self.assertTrue(
-            mock_sys_path[2].endswith('subpar/runtime/another_root'),
+            mock_sys_path[1].endswith('some_root'),
             mock_sys_path)
-        self.assertEqual(mock_sys_path[0], sys.path[0])
+        self.assertTrue(
+            mock_sys_path[2].endswith('another_root'),
+            mock_sys_path)
         # If we have no pkg_resources, or a really old version of
         # pkg_resources, setup skips some things
         module = sys.modules.get('pkg_resources', None)
         if module and support._version_check_pkg_resources(module):
             self.assertEqual(mock_sys_path[3:], sys.path[1:])
+
+    def test_setup__extract(self):
+        # Run setup() with file extraction
+        old_sys_path = sys.path
+        try:
+            mock_sys_path = list(sys.path)
+            mock_sys_path[0] = self.zipfile_name
+            sys.path = mock_sys_path
+            success = support.setup(import_roots=['some_root'], zip_safe=False)
+            self.assertTrue(success)
+        finally:
+            sys.path = old_sys_path
+
+        # Check results
+        self.assertNotEqual(mock_sys_path[0], self.zipfile_name)
+        self.assertTrue(
+            os.path.isdir(mock_sys_path[0]),
+            mock_sys_path)
 
 
 if __name__ == '__main__':
--- subpar.bzl
+++ subpar.bzl
@@ -187,13 +187,14 @@ def par_binary(name, **kwargs):
     See [py_binary](http://www.bazel.io/docs/be/python.html#py_binary)
     for arguments and usage.
     """
+    zip_safe = kwargs.pop('zip_safe', True)
     native.py_binary(name=name, **kwargs)
+
     main = kwargs.get('main', name + '.py')
     imports = kwargs.get('imports')
     default_python_version = kwargs.get('default_python_version', 'PY2')
     visibility = kwargs.get('visibility')
     testonly = kwargs.get('testonly', False)
-    zip_safe = kwargs.get('zip_safe', True)
     parfile(name=name + '.par', src=name, main=main, imports=imports,
             default_python_version=default_python_version, visibility=visibility,
             testonly=testonly, zip_safe=zip_safe)
@@ -204,13 +205,14 @@ def par_test(name, **kwargs):
     Just like par_binary, but for py_test instead of py_binary.  Useful if you
     specifically need to test a module's behaviour when used in a .par binary.
     """
+    zip_safe = kwargs.pop('zip_safe', True)
     native.py_test(name=name, **kwargs)
+
     main = kwargs.get('main', name + '.py')
     imports = kwargs.get('imports')
     default_python_version = kwargs.get('default_python_version', 'PY2')
     visibility = kwargs.get('visibility')
     testonly = kwargs.get('testonly', True)
-    zip_safe = kwargs.get('zip_safe', True)
     parfile_test(
         name=name + '.par', src=name, main=main, imports=imports,
         default_python_version=default_python_version, visibility=visibility,
